f9315f774b2081c2d2361fbd6e0c3536483aa8c9
totohpy <117004586+totohpy@users.noreply.github.com>
2025-10-08 10:13:25 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -30,7 +30,7 @@ st.markdown(
     [data-testid="stAppViewContainer"] > .main {
         background-color: #e0f2f1;
     }
-    h1 { font-size: 36px !important; }
+    h1 { font-size: 38px !important; }
     
     [data-testid="stSidebar"] {
         background-color: #e0f2f1;
