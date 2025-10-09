5c6e0470b82dc2f4208dad781069b9b9782e5c5d
totohpy <117004586+totohpy@users.noreply.github.com>
2025-10-09 13:19:58 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,18 +9,18 @@ st.set_page_config(
 # --- Add the credit to the bottom of the sidebar ---
 with st.sidebar:
      st.title("Menu")  # 🎯 เพิ่มหัวข้อ Menu
-        st.markdown("""
-            <div class="sidebar-footer">
-                <p>
-                    <span style="color: grey;">By PAO1 </span><br>
-                    <span style="font-size: 16px; letter-spacing: 0.5px;">
-                        <span style="color: red; font-weight: bold;">A</span>udit 
-                        <span style="color: red; font-weight: bold;">I</span>ntelligence
-                        <span style="color: red; font-weight: bold;">T</span>eam
-                    </span>
-                </p>
-            </div>
-    """, unsafe_allow_html=True)
+    st.markdown("""
+        <div class="sidebar-footer">
+            <p>
+                <span style="color: grey;">By PAO1 </span><br>
+                <span style="font-size: 16px; letter-spacing: 0.5px;">
+                    <span style="color: red; font-weight: bold;">A</span>udit 
+                    <span style="color: red; font-weight: bold;">I</span>ntelligence
+                    <span style="color: red; font-weight: bold;">T</span>eam
+                </span>
+            </p>
+        </div>
+""", unsafe_allow_html=True)
 # --- 🎯 คอนเทนเนอร์สำหรับรูปภาพ (จะถูกดันไปอยู่ตรงกลาง) ---
     st.markdown('<div class="sidebar-image-container">', unsafe_allow_html=True)
     try:
